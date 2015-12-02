d914c05de5a699015cadf4a01c850a42b616a417
Jeff Fowler <jeffowler@gmail.com>
2015-12-02 12:53:58 -0500

    day one part 2

--- 1.rb
+++ 1.rb
@@ -1,11 +1,21 @@
 input = File.read("input1.txt").split("")
 depth = 0
+i = 1
 input.each do |e|
     if e == "("
         depth += 1
     elsif e == ")"
         depth -=1
     end
+
+    if depth == -1 
+        @first ||= i
+        puts i
+    end
+
+    i += 1
 end
-puts depth
+
+puts "Final floor: " + depth.to_s
+puts "First basement: " + @first.to_s
 
